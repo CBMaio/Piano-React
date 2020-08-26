331da1578edc2ae3dc784bad5168849e4ef7bf37
caroMaio <carolinabelenmaio@gmail.com>
2020-08-26 17:59:57 -0300

    Updates

---
+++ precache-manifest.f3ccbabd9ffe9508cb384616141d0f2d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f278f4ba42e91745a1978a84b92714",
+    "url": "/Piano-React/index.html"
+  },
+  {
+    "revision": "18f2a74eda3e818192d1",
+    "url": "/Piano-React/static/css/main.76a1da08.chunk.css"
+  },
+  {
+    "revision": "78cccc351acf261e0bd1",
+    "url": "/Piano-React/static/js/2.4e0a0a59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Piano-React/static/js/2.4e0a0a59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18f2a74eda3e818192d1",
+    "url": "/Piano-React/static/js/main.e49b30c1.chunk.js"
+  },
+  {
+    "revision": "b21591c009b12c61ca19",
+    "url": "/Piano-React/static/js/runtime-main.c9b12a5a.js"
+  },
+  {
+    "revision": "27a22618ea0aeab156a48b6bfc3d8b5a",
+    "url": "/Piano-React/static/media/bob-esponja.27a22618.mp3"
+  },
+  {
+    "revision": "18fd3f6f1a6879d60b471eb4828bd890",
+    "url": "/Piano-React/static/media/do.18fd3f6f.wav"
+  },
+  {
+    "revision": "a787534ad7a68707ce5c7657c0a26985",
+    "url": "/Piano-React/static/media/fa.a787534a.wav"
+  },
+  {
+    "revision": "f51e9e6c4f9d30be3e6e5500020920e2",
+    "url": "/Piano-React/static/media/galaxy.f51e9e6c.jpg"
+  },
+  {
+    "revision": "81b58ea7499ac122339f3abe3237f87e",
+    "url": "/Piano-React/static/media/la-pantera-rosa.81b58ea7.mp3"
+  },
+  {
+    "revision": "10af03f3aa8a23a3e29cab5bb9f7d48f",
+    "url": "/Piano-React/static/media/la.10af03f3.wav"
+  },
+  {
+    "revision": "1f26c778a049229d1c7a2edfb789b5fd",
+    "url": "/Piano-React/static/media/mi.1f26c778.wav"
+  },
+  {
+    "revision": "217b943a565a4c60e452aac2852fd0e9",
+    "url": "/Piano-React/static/media/re.217b943a.wav"
+  },
+  {
+    "revision": "f4080db91827dfd03a7b941a35fe916e",
+    "url": "/Piano-React/static/media/recuerdame-coco.f4080db9.mp3"
+  },
+  {
+    "revision": "70924435d531fe3a2c59d6596733c5cc",
+    "url": "/Piano-React/static/media/si.70924435.wav"
+  },
+  {
+    "revision": "785a33d2df8ac21f71be64df8f8f579a",
+    "url": "/Piano-React/static/media/sol.785a33d2.wav"
+  },
+  {
+    "revision": "c8a160861064138cbc2324737c85529a",
+    "url": "/Piano-React/static/media/un-mundo-ideal-aladdin.c8a16086.mp3"
+  },
+  {
+    "revision": "2e1a86a554859fee7308f5c92499c8c1",
+    "url": "/Piano-React/static/media/yo-soy-tu-amigo-fiel-toy-story.2e1a86a5.mp3"
+  }
+]);
